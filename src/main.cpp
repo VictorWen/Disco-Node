6258380384961a48a74ff6d79852aa3aca5423b4
Victor Wen <vicwen02@g.ucla.edu>
2023-08-24 14:07:42 -0400

    Added pixel data transfer

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,28 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <DNSServer.h>
+#include <Adafruit_NeoPixel.h>
+#include <WiFiUdp.h>
 
 #include <secrets.h>
 
 #define DEBUG_LN(x) Serial.println(x)
 
+#define DISCO_PORT 12345
+#define CONFIG_PORT 12346
+#define MAX_DISCO_PACKET_SIZE 4096
+#define MAX_COLOR 255
+
 // AsyncWebServer server(80);
-AsyncServer server = AsyncServer(12346);
+AsyncServer server = AsyncServer(CONFIG_PORT);
+Adafruit_NeoPixel pixels(150, 2, NEO_GRB + NEO_KHZ800);
+WiFiUDP udp;
+byte udp_packet_buffer[MAX_DISCO_PACKET_SIZE];
+
 
 static void handleNewClient(void* arg, AsyncClient* client);
+uint32_t convert_bytes_to_int(byte* array, int offset);
+float convert_bytes_to_float(byte* array, int offset);
 
 void setup() {
     Serial.begin(115200);
@@ -58,19 +71,60 @@ void setup() {
     DEBUG_LN(MDNS.begin("chroma", WiFi.localIP()) ? "Success" : "Failure");
     
     // MDNS.addService("http", "tcp", 80);
-    auto service = MDNS.addService(0 ,"disco", "udp", 12345);
+    auto service = MDNS.addService(0 ,"disco", "udp", DISCO_PORT);
     MDNS.addServiceTxt(service, "device", "esp8266");
     MDNS.addServiceTxt(service, "version", 1);
     MDNS.addServiceTxt(service, "preferred-ipv", 4);
 
-	server.onClient(&handleNewClient, NULL);
-	server.begin();
+	// server.onClient(&handleNewClient, NULL);
+	// server.begin();
+
+    pixels.begin();
+    pixels.clear();
+    pixels.show();
+
+    udp.begin(DISCO_PORT);
 }
 
 void loop() {
 #ifdef ESP8266
     MDNS.update();
 #endif
+    int packetSize = udp.parsePacket();
+    if (packetSize > 0) {
+        // Serial.printf("Received %d bytes from %s, port %d\n", packetSize, udp.remoteIP().toString().c_str(), udp.remotePort());
+        int len = udp.read(udp_packet_buffer, MAX_DISCO_PACKET_SIZE);
+        if (len < 12) {
+            Serial.printf("Got invalid packet of length: %d, wanting 12\n", len);
+            return;
+        }
+        uint32_t start = convert_bytes_to_int(udp_packet_buffer, 0);
+        uint32_t end = convert_bytes_to_int(udp_packet_buffer, 4);
+        uint32_t n_frames = convert_bytes_to_int(udp_packet_buffer, 8);
+        // Serial.printf("Got start: %d, end: %d, n_frames: %d\n", start, end, n_frames);
+        int pixels_per_frame = end - start;
+        int total_pixels = pixels_per_frame * n_frames;
+        if (len < 12 + total_pixels * 4) {
+            Serial.printf("Got invalid packet of length: %d, wanting: %d\n", len, 12 + total_pixels * 4);
+            return;
+        }
+        // TODO: deal with multiple frames
+        for (int i = 0; i < pixels_per_frame; i++) {
+            // float a = convert_bytes_to_float(udp_packet_buffer, 24 + i * 16);
+            // uint8_t r = convert_bytes_to_float(udp_packet_buffer, 12 + i * 16) * a * MAX_COLOR;
+            // uint8_t g = convert_bytes_to_float(udp_packet_buffer, 16 + i * 16) * a * MAX_COLOR;
+            // uint8_t b = convert_bytes_to_float(udp_packet_buffer, 20 + i * 16) * a * MAX_COLOR;
+
+            uint8_t r = udp_packet_buffer[12 + i * 4];
+            uint8_t g = udp_packet_buffer[13 + i * 4];
+            uint8_t b = udp_packet_buffer[14 + i * 4];
+            uint8_t a = udp_packet_buffer[15 + i * 4];
+            
+            // Serial.printf("Pixel color at %d is %d, %d, %d, %f\n", start + i, r, g, b, a);
+            pixels.setPixelColor(start + i, r, g, b);
+        }
+        pixels.show();
+    }
 }
 
 static void handleData(void* arg, AsyncClient* client, void *data, size_t len) {
@@ -89,4 +143,30 @@ static void handleNewClient(void* arg, AsyncClient* client) {
 	
 	// register events
 	client->onData(&handleData, NULL);;
+}
+
+uint32_t convert_bytes_to_int(byte* array, int offset) {
+    union byte_int {
+        byte array[4];
+        uint32_t value;
+    };
+    byte_int data;
+    data.array[0] = array[offset + 0];
+    data.array[1] = array[offset + 1];
+    data.array[2] = array[offset + 2];
+    data.array[3] = array[offset + 3];
+    return data.value;
+}
+
+float convert_bytes_to_float(byte* array, int offset) {
+    union byte_float {
+        byte array[4];
+        float value;
+    };
+    byte_float data;
+    data.array[0] = array[offset + 0];
+    data.array[1] = array[offset + 1];
+    data.array[2] = array[offset + 2];
+    data.array[3] = array[offset + 3];
+    return data.value;
 }
